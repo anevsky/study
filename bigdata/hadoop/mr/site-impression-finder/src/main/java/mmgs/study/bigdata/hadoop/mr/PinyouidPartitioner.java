bb7326b2927cf3d7c8ee66d01ea934ee1c42cfbd
m-m-g-s <m.m.g.study@gmail.com>
2016-09-06 12:31:10 +0300

    [MR-SI-Finder] partitioner implemented

--- bigdata/hadoop/mr/site-impression-finder/src/main/java/mmgs/study/bigdata/hadoop/mr/PinyouidPartitioner.java
+++ bigdata/hadoop/mr/site-impression-finder/src/main/java/mmgs/study/bigdata/hadoop/mr/PinyouidPartitioner.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.mapreduce.Partitioner;
 
 public class PinyouidPartitioner extends Partitioner<PinyouidTimestampWritable, NullWritable> {
     @Override
-    public int getPartition(PinyouidTimestampWritable pinyouidTimestampWritable, NullWritable nullWritable, int i) {
-        throw new UnsupportedOperationException("Not implemented yet!");
+    public int getPartition(PinyouidTimestampWritable p, NullWritable nullWritable, int i) {
+        return (p.getPinyouid().hashCode() * 137) % i;
     }
 }
--- bigdata/hadoop/mr/site-impression-finder/src/test/java/mmgs/study/bigdata/hadoop/mr/PinyouidPartitionerTest.java
+++ bigdata/hadoop/mr/site-impression-finder/src/test/java/mmgs/study/bigdata/hadoop/mr/PinyouidPartitionerTest.java
@@ -1,13 +1,33 @@
 package mmgs.study.bigdata.hadoop.mr;
 
+import org.apache.hadoop.io.NullWritable;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.*;
 
 public class PinyouidPartitionerTest {
-    @Test
-    public void getPartition() throws Exception {
+    private PinyouidPartitioner partitioner = new PinyouidPartitioner();
+    private NullWritable nothing = NullWritable.get();
+    private int partitionsAmt = 2;
 
+    @Test
+    public void getPartitionFirstDifferent() throws Exception {
+        PinyouidTimestampWritable record1 = new PinyouidTimestampWritable("Vh1_O55PDtSUDQb", "20130606000829000");
+        PinyouidTimestampWritable record2 = new PinyouidTimestampWritable("ZYqzZA5EDlzWqt", "20130606001605800");
+        int partition1 = partitioner.getPartition(record1, nothing, partitionsAmt);
+        int partition2 = partitioner.getPartition(record2, nothing, partitionsAmt);
+        assertThat("Different records get to different partitions", partition1, not(equalTo(partition2)));
+    }
+
+    @Test
+    public void getPartitionSecondDifferent() throws Exception {
+        PinyouidTimestampWritable record1 = new PinyouidTimestampWritable("Vh1_O55PDtSUDQb", "20130606000829000");
+        PinyouidTimestampWritable record2 = new PinyouidTimestampWritable("Vh1_O55PDtSUDQb", "20130606001605800");
+        int partition1 = partitioner.getPartition(record1, nothing, partitionsAmt);
+        int partition2 = partitioner.getPartition(record2, nothing, partitionsAmt);
+        assertThat("Records with equal pinyouid get to the same partition", partition1, equalTo(partition2));
     }
 
 }
